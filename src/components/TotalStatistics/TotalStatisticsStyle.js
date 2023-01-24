d5dbd4ece33f846da137cf7a2d64b6dcdfa1c762
Iuliia Sokol <avare779@gmail.com>
2023-01-24 16:29:27 +0200

    stats bar styles edited

--- src/components/TotalStatistics/TotalStatisticsStyle.js
+++ src/components/TotalStatistics/TotalStatisticsStyle.js
@@ -3,16 +3,21 @@ import { theme } from 'components/Theme';
 
 const TotalStatsStyle = {
   BoxStats: styled.div`
+    box-sizing: border-box;
+    height: 85px;
+    padding: 20px 10px;
     display: block;
     text-align: center;
-    width: 705px;
-    padding: 15px;
-    border-radius: 30px;
     margin: 0 auto 30px auto;
     background-color: ${theme.colors.PrimaryWhite};
-    @media screen and (max-width: 768px) {
-      width: 100%;
-      padding: 5px 5px;
+    box-shadow: 5px 10px 20px rgba(170, 178, 197, 0.4);
+    border-radius: 20px;
+
+    @media screen and (min-width: 768px) {
+      height: 50px;
+      width: 705px;
+      padding: 15px;
+      border-radius: 30px;
     }
 
     @media screen and (min-width: 1280px) {
@@ -20,40 +25,72 @@ const TotalStatsStyle = {
     }
   `,
   List: styled.ul`
+    height: 100%;
     display: flex;
     padding: 0;
     margin: 0;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     list-style: none;
     font-weight: 700;
     font-size: 14px;
     line-height: 16px;
     color: ${theme.colors.TextGray};
+
     @media screen and (max-width: 768px) {
+      justify-content: center;
       column-gap: 10px;
       width: 100%;
     }
   `,
+
   AmountExpenses: styled.li`
     width: 50%;
+    height: 100%;
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
     & > span {
       color: red;
-      margin-left: 15px;
       display: inline-block;
+    }
+
+    @media screen and (min-width: 768px) {
+      flex-direction: row;
+      padding-right: 20px;
+      justify-content: flex-end;
+
+      & > span {
+        margin-left: 15px;
+      }
     }
   `,
+
   AmountIncome: styled.li`
     width: 50%;
+    height: 100%;
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border-left: 1px solid ${theme.colors.TextGray};
+
     & > span {
       color: green;
-      margin-left: 15px;
       display: inline-block;
     }
-    @media screen and (max-width: 768px) {
-      border-left: 1px solid ${theme.colors.TextGray};
+
+    @media screen and (min-width: 768px) {
+      flex-direction: row;
+      padding-left: 20px;
+
+      & > span {
+        margin-left: 15px;
+      }
     }
   `,
 };
